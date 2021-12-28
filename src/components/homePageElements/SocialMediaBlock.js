ee0ca82abce1d4dfa28220cde126546b077ddd65
Pranit Sharma <pranitsharma@Pranits-MacBook.local>
2021-12-28 17:15:12 +0530

    Social Media Icons - Home Screen

--- src/components/homePageElements/SocialMediaBlock.js
+++ src/components/homePageElements/SocialMediaBlock.js
@@ -3,11 +3,11 @@ import "./SocialMediaBlock.css";
 function SocialMediaBlock(props) {
   return (
     <div className="social-block-home">
-      <i className="fab fa-1.5x fa-facebook"></i>
-      <i className="fab fa-1.5x fa-instagram"></i>
-      <i className="fab fa-1.5x fa-youtube"></i>
-      <i className="fab fa-1.5x fa-whatsapp"></i>
-    </div>
+      <a href="https://facebook.com"><i className="fab fa-lg fa-facebook"></i></a>
+      <a href="https://instagram.com"><i className="fab fa-lg fa-instagram"></i></a>
+      <a href="https://youtube.com"><i className="fab fa-lg fa-youtube"></i></a>
+      <a href="https://whatsapp.com"><i className="fab fa-lg fa-whatsapp"></i></a>
+    </div >
   );
 }
 
