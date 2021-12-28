7bc8ef40d327851bd0393a7ef8a6780a3cfaa3bb
Pranit Sharma <pranitsharma@Pranits-MacBook.local>
2021-12-28 14:27:02 +0530

    Changing Image URL on Home Page

--- src/components/homePageElements/ImageCard.js
+++ src/components/homePageElements/ImageCard.js
@@ -6,7 +6,7 @@ function ImageCard(props) {
       <img
         alt="mainImage"
         src={
-          "https://res.cloudinary.com/aaryaprasannan/image/upload/v1626926927/PopArt/BeforeSunrise1995.jpg"
+          "https://ritikasinghin.files.wordpress.com/2021/08/bacf3957-da78-4897-82fa-2be2836bc93e-4.jpg?w=754"
         }
       />
     </div>
