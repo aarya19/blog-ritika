3f4e072f42a2dbe32044b111805c28d2710d4ccf
Aarya Prasannan <aaryaprasannan@Aaryas-MacBook-Air.local>
2021-12-28 19:17:37 +0530

    Contact form

--- src/components/bookSessionElements/BookingForm.js
+++ src/components/bookSessionElements/BookingForm.js
@@ -1,8 +1,33 @@
+import "./BookingForm.css";
+
 function BookingForm(props) {
   return (
-    <div>
-      <h1>PERSONAL DETAILS</h1>
-      <form></form>
+    <div className="form-container">
+      <h1>Personal Details</h1>
+
+      <form>
+        <div className="form-inputs">
+          <input type={"text"} placeholder="First Name"></input>
+
+          <input type={"text"} placeholder="Last Name"></input>
+
+          <input type={"number"} placeholder="Phone number"></input>
+
+          <input type={"email"} placeholder="Email id"></input>
+
+          <input type={"text"} placeholder="Address"></input>
+
+          <input type={"number"} placeholder="Age"></input>
+        </div>
+
+        <div className="form-text-input">
+          <h2>
+            Please mention any details you would like me to know prior to the
+            session:
+          </h2>
+          <input type={"textarea"}></input>
+        </div>
+      </form>
     </div>
   );
 }
