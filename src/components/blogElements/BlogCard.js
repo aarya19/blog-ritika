513cb57d585aa4d730f23f0bc72ccc91f0b83947
Aarya Prasannan <aaryaprasannan@Aaryas-MacBook-Air.local>
2021-12-30 10:47:57 +0530

    Blog card styling

--- src/components/blogElements/BlogCard.js
+++ src/components/blogElements/BlogCard.js
@@ -3,11 +3,22 @@ import "./BlogCard.css";
 function BlogCard(props) {
   return (
     <div className="blog-card-container">
-      <div></div>
+      <div className="blog-img-container">
+        <img
+          src={
+            "https://ritikasinghin.files.wordpress.com/2021/08/bacf3957-da78-4897-82fa-2be2836bc93e-4.jpg?w=754"
+          }
+        ></img>
+      </div>
       <p>December 19, 2021</p>
       <div>
         <h2>Title of the Blog</h2>
-        <p>Small description</p>
+        <p>
+          This article has talks about Mental Health which is vital for a
+          healthy life. This article has talks about Mental Health which is
+          vital for a healthy life. This article has talks about Mental Health
+          which is vital for a healthy life.
+        </p>
       </div>
     </div>
   );
