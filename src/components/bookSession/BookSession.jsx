e5af7270a79db5b87ad46a7f433b787ef26b033d
Aarya Prasannan <aaryaprasannan@Aaryas-Air.bbrouter>
2022-01-18 00:18:22 +0530

    Book a session alignment

--- src/components/bookSession/BookSession.jsx
+++ src/components/bookSession/BookSession.jsx
@@ -1,32 +1,33 @@
-import './bookSession.css';
+import "./bookSession.css";
 
 const BookASession = (props) => {
-
   return (
-    <div className="form-container">
-      <h1>Personal Details</h1>
+    <div className="book-session-container">
+      <div className="form-container">
+        <h1>Personal Details</h1>
 
-      <form>
-        <div className="form-inputs">
-          <input type={"text"} placeholder="First Name"></input>
-          <input type={"text"} placeholder="Last Name"></input>
-          <input type={"number"} placeholder="Phone number"></input>
-          <input type={"email"} placeholder="Email id"></input>
-          <input type={"text"} placeholder="Address"></input>
-          <input type={"number"} placeholder="Age"></input>
-        </div>
+        <form>
+          <div className="form-inputs">
+            <input type={"text"} placeholder="First Name"></input>
+            <input type={"text"} placeholder="Last Name"></input>
+            <input type={"number"} placeholder="Phone number"></input>
+            <input type={"email"} placeholder="Email id"></input>
+            <input type={"text"} placeholder="Address"></input>
+            <input type={"number"} placeholder="Age"></input>
+          </div>
 
-        <div className="form-text-input">
-          <h2>
-            Please mention any details you would like me to know prior to the
-            session:
-          </h2>
-          <input type={"textarea"}></input>
-          <button typeof="submit">SUBMIT</button>
-        </div>
-      </form>
+          <div className="form-text-input">
+            <h2>
+              Please mention any details you would like me to know prior to the
+              session:
+            </h2>
+            <input type={"textarea"}></input>
+            <button typeof="submit">SUBMIT</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
-}
+};
 
 export default BookASession;
--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -2,30 +2,45 @@ import "./Navbar.css";
 import { NavLink } from "react-router-dom";
 
 function Header(props) {
-  
   const logoClass = props.home ? "logoHome" : "logo";
 
-  const bookingClass = props.book ? "side-links-li session-button-booking" : "side-links-li session-button";
+  const bookingClass = props.book
+    ? "side-links-li session-button-booking"
+    : "side-links-li session-button";
 
   return (
-    <header>
-      <div className={logoClass}>
-        <NavLink to="/" className="side-links-li" activeClassName="side-links-li active">
-          <h1>R</h1>
-        </NavLink>
-      </div>
-      <ul className="side-links">
-        <NavLink to="/blog" className="side-links-li" activeClassName="side-links-li active">
-          Blog
-        </NavLink>
-        <NavLink to="/about" className="side-links-li">
-          About Me
-        </NavLink>
-        <NavLink to="/book" className={bookingClass} activeClassName="side-links-li active">
-          Book a session
-        </NavLink>
-      </ul>
-    </header>
+    <div className="nav-container">
+      <header>
+        <div className={logoClass}>
+          <NavLink
+            to="/"
+            className="side-links-li"
+            activeClassName="side-links-li active"
+          >
+            <h1>R</h1>
+          </NavLink>
+        </div>
+        <ul className="side-links">
+          <NavLink
+            to="/blog"
+            className="side-links-li"
+            activeClassName="side-links-li active"
+          >
+            Blog
+          </NavLink>
+          <NavLink to="/about" className="side-links-li">
+            About Me
+          </NavLink>
+          <NavLink
+            to="/book"
+            className={bookingClass}
+            activeClassName="side-links-li active"
+          >
+            Book a session
+          </NavLink>
+        </ul>
+      </header>
+    </div>
   );
 }
 
